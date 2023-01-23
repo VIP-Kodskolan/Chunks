0d7742544023e1299d4c00d275eec0cb126a6fdb
erikpineiro <erikpineiro@gmail.com>
2023-01-23 13:47:18 +0100

    test

--- initializer.js
+++ initializer.js
@@ -1,5 +1,5 @@
 // Created automatically at startup by index.php
-// test
+// test branch
 
 import content_chapter_list from './js/components/content_chapter_list.js';
 import content_chapter_list_item from './js/components/content_chapter_list_item.js';
