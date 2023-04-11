de5b4885524ea2b244007ab14778687e679f4dce
LiamHjalmarsson <Liam.hjalmarsson@hotmail.com>
2023-04-11 19:15:07 +0200

    sa

--- js/components/filter_chapters.js
+++ js/components/filter_chapters.js
@@ -1,6 +1,6 @@
 import state_io from "../utils/state_io.js";
 import { SubPub } from "../utils/subpub.js";
-import utils from "../utils/utils.js"
+import utils from "../utils/utils.js";
 
 export default { }
 
@@ -11,16 +11,12 @@ export default { }
         listener: render,
     });
 
-    SubPub.subscribe({
-        event: "db::patch::users_units::done",
-        listener: render,
-    });
-
 })();
 
-function render () {
-    let content_course_open = document.querySelector("#content_course_open");
+function render (data) {
+    let { response } = data;
 
+    let content_course_open = document.querySelector("#content_course_open");
     content_course_open.querySelectorAll("div").forEach(div => {
         if (div.id === "filterBtnsContainer") { 
             div.remove();
@@ -34,15 +30,21 @@ function render () {
     let btnsFiltering = [
         {
             text: "Completed",
-            filter: filterCompletd
+            filter: () => {
+                filterCompletd(response);
+            }
         },
         {
             text: "UnCompleted",
-            filter: filterUnCompleted
+            filter: () => {
+                filterUnCompleted(response)
+            }
         },
         {
             text: "Questions",
-            filter: filterQuestions
+            filter: () => {
+                filterQuestions(response)
+            }
         },
     ];
 
@@ -55,7 +57,7 @@ function render () {
             filterBtnsContainer.querySelectorAll("button").forEach(btn => {
                 if (e.target != btn) {
                     btn.classList.remove("Filteractive");
-                    showAll();
+                    showAll(response);
                 }
             });
 
@@ -64,16 +66,18 @@ function render () {
             if (btn.classList.contains("Filteractive")) {
                 filter.filter();
             } else {
-                showAll();
+                showAll(response);
             }
-        });
-        filterBtnsContainer.appendChild(btn);
-    });
 
+        });
+
+        filterBtnsContainer.appendChild(btn);
+
+    });
 }
 
-function filterQuestions() {
-    let { chapters, users_units  } = state_io.state;
+function filterQuestions(response) {
+    let { chapters, users_units  } = response;
 
     let questions = users_units.filter(unit => unit.check_question);
 
@@ -88,11 +92,10 @@ function filterQuestions() {
 
 }
 
-function filterCompletd () {
-    let { chapters, units } = state_io.state;
+function filterCompletd (response) {
+    let { chapters, units } = response;
 
     let incompleteChapters = chapters.filter(chapter => {
-
         let unitsInChapter = units.filter(unit => unit.chapter_id === chapter.chapter_id);
 
         let allUnitsComplete = unitsInChapter.every(unit => unit.check_complete);
@@ -104,13 +107,13 @@ function filterCompletd () {
         let liElement = document.querySelector(`#chapter_list_id_${chapter.chapter_id}`);
         liElement.style.display = "none";
     });
+
 }
 
-function filterUnCompleted () {
-    let { chapters, units } = state_io.state;
+function filterUnCompleted (response) {
+    let { chapters, units } = response;
 
     let incompleteChapters = chapters.filter(chapter => {
-
         let unitsInChapter = units.filter(unit => unit.chapter_id === chapter.chapter_id);
 
         let allUnitsComplete = unitsInChapter.some(unit => !unit.check_complete);
@@ -118,19 +121,17 @@ function filterUnCompleted () {
         return allUnitsComplete === false;
     });
     
-    console.log(incompleteChapters)
     incompleteChapters.forEach(chapter => {
         let liElement = document.querySelector(`#chapter_list_id_${chapter.chapter_id}`);
         liElement.style.display = "none";
     });
 }
 
-function showAll () {
-    const { chapters } = state_io.state;
+function showAll (response) {
+    const { chapters } = response;
 
     chapters.forEach(chapter => {
         let liElement = document.querySelector(`#chapter_list_id_${chapter.chapter_id}`);
         liElement.style.display = "block";
     }) 
-
 }
